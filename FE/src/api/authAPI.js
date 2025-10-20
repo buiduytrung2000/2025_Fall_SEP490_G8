dade7af6bd62110407c9f8fa36e6ba1f17c877b0
ManhNTHE170803 <manhnthe170803@fpt.edu.vn>
2025-10-21 00:14:28 +0700

    fix node module
    anh em npm i và npm start là chạy nhé

--- FE/src/api/authAPI.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "./axiosInstance";
-
-const authAPI = {
-    login: (data) => axiosInstance.post("/auth/login", data),
-    forgotPassword: (email) => axiosInstance.post("/auth/forgot-password", { email }),
-    resetPassword: (token, newPassword) => axiosInstance.post("/auth/reset-password", { token, newPassword }),
-};
-
-export default authAPI;
--- FE/src/constants/roles.js
+++
@@ -1,5 +0,0 @@
-export const ROLES = {
-    ADMIN: 'admin',
-    STAFF: 'staff',
-    TENANT: 'tenant'
-};
